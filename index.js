320d5aa96a447d6d007f36d6e0fee44d553d77c3
kerwindows <kerwindows@hotmail.com>
2022-09-17 07:48:17 -0400

    Working API with no lint errors, but with comments

--- index.js
+++ index.js
@@ -5,17 +5,15 @@ const app = express();
 const usersRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 
-// const { PORT = 3000 } = process.env;
+const { PORT = 3000 } = process.env;
 
-// app.listen(PORT, () => {
-//   console.log(`Listening on port ${PORT}`);
-// });
 app.use('/users', usersRouter);
 app.use('/cards', cardRouter);
-
 app.use((req, res) => {
   res.status(404).send({ message: 'Requested resource not found' });
 });
+
+app.listen(PORT, () => {});
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!');
